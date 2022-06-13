931012d29ba460683ab3fffa3069e0435084bbab
Adeniran Oluwaseun Adebola <adeniranoluwaseun0608@gmail.com>
2022-06-13 14:09:36 -0700

    Points to an integer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * reset_to_98 - Takes a pointer to an integer and upgrades the value .
+ * @n: The pointer to an integer.
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+

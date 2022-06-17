d8787e0fa06b6c8632953d7aa655c894d2bbdf00
Adeniran Oluwaseun Adebola <adeniranoluwaseun0608@gmail.com>
2022-06-16 21:50:31 -0700

    Cancatenate two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,4 @@
-#include "stdio.h"
+#include<stdio.h>
 /**
  * _strcat - concatenates two strings,
  * @dest: destination.
@@ -24,7 +24,4 @@ char *_strcat(char *dest, char *src)
 	}
 	return (dest);
 }
-
-
-
 
